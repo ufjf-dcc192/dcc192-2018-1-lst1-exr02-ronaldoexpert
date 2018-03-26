c0c65efe0b8b62fc241499cdc0808a1783f80bef
ice <ice@lab03m24.nrc.ice.ufjf.br>
2018-03-26 19:37:18 -0300

    Crição e preenchimento do HashMap

--- src/java/PaisesServlet.java
+++ src/java/PaisesServlet.java
@@ -4,7 +4,9 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,62 +14,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "ListaPessoasServlet", urlPatterns = {"/index.html"})
-public class PaisesServlet extends HttpServlet implements Comparator<String>{
+public class PaisesServlet extends HttpServlet{
 
-    private List<String> frutas;
+    private Map<String, String> paises = new HashMap<String, String>();
 
-    public PaisesServlet() {
-        frutas = new ArrayList<String>();
-        frutas.add("Banana");
-        frutas.add("Laranja");
-        frutas.add("Limao");
-        frutas.add("Maca");
-        frutas.add("Maracuja");
-        frutas.add("Abacaxi");
-    }
-    
-    @Override
-    public int compare(String a, String b){        
-        return a.length() - b.length();
-    }
-        
+    public PaisesServlet() {        
+        paises.put("Brasil", "Azul");
+        paises.put("Australia", "Amarelo");
+        paises.put("Estados Unidos", "Vermelho");
+        paises.put("Japão", "Branco");
+        paises.put("Colombia", "Verde");
+    }        
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         try (PrintWriter out = response.getWriter()) {
-            String comand = "alfa";
+            String comand = "";
             comand = request.getParameter("comando");
 
-            if("alfa".equals(comand)){
-                Collections.sort(frutas);        
-            }else if ("tam".equals(comand)){
-                //Collections.sort(frutas, (a,b)-> a.length()-b.length());
-                Collections.sort(frutas, this);
-            }else{
-                Collections.shuffle(frutas);
-            }
-            
-        
             response.setContentType("text/html;charset=UTF-8");
             
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Pessoas</title>");
+            out.println("<title>Países</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Frutas</h1>");
             out.println("<ul>");            
-
-            for (String frutas : frutas) {
-                out.println("<li>" + frutas + "</li>");
-            }
-            
+            for (int i = 0; i < paises.size(); i++) {
+                out.println("<li>" + paises.get(i).toString() + "</li>");
+            }            
             out.println("</ul>");
-            out.println("<a href = '?comando=alfa'>Ordem Alfabetica</a><BR>");            
-            out.println("<a href = '?comando=rand'>Ordem Aleatória</a><BR>");
-            out.println("<a href = '?comando=tam'>Ordem Tamanho Palavra</a><BR>");
+            //out.println("<a href = '?comando=alfa'>Ordem Alfabetica</a><BR>");            
+            //out.println("<a href = '?comando=rand'>Ordem Aleatória</a><BR>");
+            //out.println("<a href = '?comando=tam'>Ordem Tamanho Palavra</a><BR>");
             out.println("</body>");
             out.println("</html>");
         }       
